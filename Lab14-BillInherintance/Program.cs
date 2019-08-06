cc7195edc44f805c8d770fb6d6eb1c5a5f7b7ce5
Lexi <lexa.shelby@gmail.com>
2019-08-06 15:27:32 -0400

    lab 14 first commit

---
+++ Lab14-BillInherintance/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab14_BillInherintance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Bill bill = new Bill(15.80, 0.06);
+            TippableBill tippableBill = new TippableBill(2.0, 8.50, 0.06);
+
+            Pay(bill);
+            Pay(tippableBill);
+        }
+        public static void Pay(Bill bill)
+        {
+            Console.WriteLine($"${bill.CalcTotal()}");
+        }
+    }
+}
---
+++ Lab14-BillInherintance/TippableBill.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab14_BillInherintance
+{
+    class TippableBill:Bill
+    {
+        public double Tip { get; set; }
+
+        public TippableBill()
+        {
+        }
+
+        public TippableBill(double tip, double subtotal, double taxRate):base(subtotal, taxRate)
+        {
+            Tip = tip;
+            Subtotal = subtotal;
+            TaxRate = taxRate;
+        }
+        public override double CalcTotal()
+        {
+            return (Tip + (Subtotal * (1 + TaxRate)));
+        }
+    }
+}
